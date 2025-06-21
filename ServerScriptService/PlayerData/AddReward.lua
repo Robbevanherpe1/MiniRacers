55e5e8ae6610bead0ce7073bf453322624bb6a5f
Robbe <robbe.van.herpe@outlook.com>
2025-06-21 15:13:55 +0200

    Updated ServerScriptService/PlayerData/AddReward.lua

--- ServerScriptService/PlayerData/AddReward.lua
+++ ServerScriptService/PlayerData/AddReward.lua
@@ -1,14 +1,10 @@
 -- @ScriptType: ModuleScript
 return function(player, position)
 	local stats = player:FindFirstChild("leaderstats")
-
 	if not stats then return end
-
 	local coins = stats:FindFirstChild("Coins")
 	local level = stats:FindFirstChild("Level")
-	
 	if not coins or not level then return end
-
 	if position == 1 then
 		coins.Value += 5
 		level.Value += 0.5
@@ -22,7 +18,6 @@ return function(player, position)
 		coins.Value += 1
 		level.Value += 0.1
 	end
-	
 	local AddRewardEvent = game.ReplicatedStorage:WaitForChild("AddReward")
 	AddRewardEvent:FireClient(player)
 end
