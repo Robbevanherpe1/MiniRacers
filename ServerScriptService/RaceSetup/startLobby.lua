3f729e0e8de36c2e89690d4e4f6990c464e65312
Robbe <robbe.van.herpe@outlook.com>
2025-06-21 15:53:12 +0200

    Updated ServerScriptService/RaceSetup/startLobby.lua

--- ServerScriptService/RaceSetup/startLobby.lua
+++ ServerScriptService/RaceSetup/startLobby.lua
@@ -12,7 +12,11 @@ end
 local s3 = workspace.Map1.Race_start["3"].uni0033
 local s2 = workspace.Map1.Race_start["2"].uni0032
 local s1 = workspace.Map1.Race_start["1"].uni0031
+
+local countdownStarted = false
+
 local function runCountdown()
+	countdownStarted = true
 	task.wait(5)
 	s3.Transparency, s2.Transparency, s1.Transparency = 0,1,1
 	task.wait(1.2)
@@ -21,14 +25,20 @@ local function runCountdown()
 	s3.Transparency, s2.Transparency, s1.Transparency = 1,1,0
 	task.wait(1.2)
 	s3.Transparency, s2.Transparency, s1.Transparency = 1,1,1
+	countdownStarted = false
 	startRaceEvent:FireAllClients()
 end
 local function onJoin(player)
-	player:SetAttribute("Placement", nil)
+	for _, p in ipairs(Players:GetPlayers()) do
+		p:SetAttribute("Placement", nil)
+	end
 	player:SetAttribute("RaceMode", 1)
 	RaceData.add(player)
 	if RaceData.getCount() >= 1 then
-		runCountdown()
+		
+		if not countdownStarted then
+			runCountdown()
+		end
 	end
 end
 joinEvent.OnServerEvent:Connect(onJoin)
