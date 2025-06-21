649a879de49fa223c4d2b9d17b6abd989e88e926
Robbe <robbe.van.herpe@outlook.com>
2025-06-21 15:13:59 +0200

    Updated ServerScriptService/RaceSetup/Cleanup.lua

--- ServerScriptService/RaceSetup/Cleanup.lua
+++ ServerScriptService/RaceSetup/Cleanup.lua
@@ -1,13 +1,10 @@
 -- @ScriptType: Script
 local Players = game:GetService("Players")
 local Workspace = game:GetService("Workspace")
-
 Players.PlayerRemoving:Connect(function(player)
 	local carName = player.Name .. "_Car"
 	local car = Workspace:FindFirstChild(carName)
-
 	if car and car:IsA("Model") then
 		car:Destroy()
-		print("[Cleanup] Removed car for", player.Name)
 	end
 end)
