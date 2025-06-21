6e047a12aff7d21aa271cf20adb02b72eeba2327
Robbe <robbe.van.herpe@outlook.com>
2025-06-21 14:28:07 +0200

    Updated ServerScriptService/RaceControl/RaceData.lua

---
+++ ServerScriptService/RaceControl/RaceData.lua
@@ -0,0 +1,33 @@
+-- @ScriptType: ModuleScript
+local RaceData = {count = 0, racers = {}, placement = 0}
+
+function RaceData.add(player)
+	if not RaceData.racers[player] then
+		RaceData.racers[player] = true
+		RaceData.count += 1
+	end
+end
+
+function RaceData.remove(player)
+	if RaceData.racers[player] then
+		RaceData.racers[player] = nil
+		RaceData.count -= 1
+	end
+end
+
+function RaceData.getCount()
+	return RaceData.count
+end
+
+function RaceData.reset()
+	RaceData.count = 0
+	RaceData.racers = {}
+	RaceData.placement = 0
+end
+
+function RaceData.nextPlacement()
+	RaceData.placement += 1
+	return RaceData.placement
+end
+
+return RaceData
