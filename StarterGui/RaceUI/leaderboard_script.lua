2522ce4742d0dfff356cae3db2258008ef1aeee0
Robbe <robbe.van.herpe@outlook.com>
2025-06-21 14:28:04 +0200

    Updated StarterGui/RaceUI/leaderboard_script.lua

---
+++ StarterGui/RaceUI/leaderboard_script.lua
@@ -0,0 +1,75 @@
+-- @ScriptType: LocalScript
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local updateEvent = ReplicatedStorage:WaitForChild("UpdateLeaderboard")
+local Players = game:GetService("Players")
+local localPlayer = Players.LocalPlayer
+
+local leaderboardFrame = script.Parent:WaitForChild("Leaderboard"):WaitForChild("leaderboard")
+
+local nameLabels = {
+	leaderboardFrame:WaitForChild("p1"):WaitForChild("name"),
+	leaderboardFrame:WaitForChild("p2"):WaitForChild("name"),
+	leaderboardFrame:WaitForChild("p3"):WaitForChild("name"),
+	leaderboardFrame:WaitForChild("p4"):WaitForChild("name"),
+	leaderboardFrame:WaitForChild("p5"):WaitForChild("name"),
+}
+
+local finishLabels = {
+	leaderboardFrame:WaitForChild("p1"):WaitForChild("Finished"),
+	leaderboardFrame:WaitForChild("p2"):WaitForChild("Finished"),
+	leaderboardFrame:WaitForChild("p3"):WaitForChild("Finished"),
+	leaderboardFrame:WaitForChild("p4"):WaitForChild("Finished"),
+	leaderboardFrame:WaitForChild("p5"):WaitForChild("Finished"),
+}
+
+local placementLabels = {
+	leaderboardFrame:WaitForChild("p1"):WaitForChild("Place"),
+	leaderboardFrame:WaitForChild("p2"):WaitForChild("Place"),
+	leaderboardFrame:WaitForChild("p3"):WaitForChild("Place"),
+	leaderboardFrame:WaitForChild("p4"):WaitForChild("Place"),
+	leaderboardFrame:WaitForChild("p5"):WaitForChild("Place"),
+}
+
+local imgLabels = {
+	leaderboardFrame:WaitForChild("p1"):WaitForChild("ImageLabel"),
+	leaderboardFrame:WaitForChild("p2"):WaitForChild("ImageLabel"),
+	leaderboardFrame:WaitForChild("p3"):WaitForChild("ImageLabel"),
+	leaderboardFrame:WaitForChild("p4"):WaitForChild("ImageLabel"),
+	leaderboardFrame:WaitForChild("p5"):WaitForChild("ImageLabel"),
+}
+
+local currentLapLabel = leaderboardFrame:WaitForChild("CurentLap")
+
+local function updateLeaderboard(data)
+	for i = 1, 5 do
+		local info = data[i]
+		if info then
+			local plr = Players:FindFirstChild(info.Name)
+			local placement = plr and plr:GetAttribute("Placement")
+			if placement then
+				nameLabels[i].Text = info.Name
+				finishLabels[i].Visible = true
+				
+			else
+				nameLabels[i].Text = info.Name
+				finishLabels[i].Visible = false
+			end
+			placementLabels[i].Visible = true
+			imgLabels[i].Image = "https://www.roblox.com/headshot-thumbnail/image?userId=" .. info.UserId .. "&width=420&height=420&format=png"
+			imgLabels[i].Visible = true
+		else
+			nameLabels[i].Text = ""
+			imgLabels[i].Visible = false
+			placementLabels[i].Visible = false
+		end
+	end
+	for _, info in ipairs(data) do
+		if info.UserId == localPlayer.UserId then
+			currentLapLabel.Text = "L " .. info.Lap .. "/3"
+			break
+		end
+	end
+end
+
+
+updateEvent.OnClientEvent:Connect(updateLeaderboard)
