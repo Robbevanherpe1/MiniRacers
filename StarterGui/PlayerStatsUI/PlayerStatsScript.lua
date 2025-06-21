f08d627599535cab15844c41e344ae01a76fd512
Robbe <robbe.van.herpe@outlook.com>
2025-06-21 15:14:00 +0200

    Updated StarterGui/PlayerStatsUI/PlayerStatsScript.lua

--- StarterGui/PlayerStatsUI/PlayerStatsScript.lua
+++ StarterGui/PlayerStatsUI/PlayerStatsScript.lua
@@ -6,16 +6,10 @@ local playerNameIcon = script.Parent:WaitForChild("stats"):WaitForChild("PlayerN
 local stats = player:WaitForChild("leaderstats")
 local coins = stats:WaitForChild("Coins")
 local AddRewardEvent = game.ReplicatedStorage:WaitForChild("AddReward")
-
 playerIcon.Image = "https://www.roblox.com/headshot-thumbnail/image?userId=" .. player.UserId .. "&width=420&height=420&format=png"
 playerNameIcon.Text = player.Name
 cashicon.Text = coins.Value
-
-
 local function UpdateCoins()
 	cashicon.Text = coins.Value
 end
-
-
-
 AddRewardEvent.OnClientEvent:Connect(UpdateCoins)
